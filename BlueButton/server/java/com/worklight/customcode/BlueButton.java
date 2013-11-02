6cfc80d063f43b847e90edfbe9d6452c6e7c74c3
Benjamin Fletcher <benjaminfletcher@Benjamins-MacBook-Pro.local>
2013-11-02 11:53:37 -0400

    third push

---
+++ BlueButton/server/java/com/worklight/customcode/BlueButton.java
@@ -0,0 +1,19 @@
+/*
+ *  Licensed Materials - Property of IBM
+ *  5725-G92 (C) Copyright IBM Corp. 2011, 2013. All Rights Reserved.
+ *  US Government Users Restricted Rights - Use, duplication or
+ *  disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ */
+package com.worklight.customcode;
+
+import java.util.logging.Logger;
+
+public class BlueButton {
+	
+	private final static Logger logger = Logger.getLogger(BlueButton.class.getName());
+	
+	public static int parseData(String theData){
+		return 1;
+	}
+	
+}
